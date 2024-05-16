c7b2a50c26f6e81bba3abb0d10cb6b2087ebe195
Christian Repnik <kris18star@gmail.com>
2024-05-16 17:07:37 +0300

    Order Service

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context) => _context = context;
+
+        public Task<Order> GetOrder()
+        {
+            return _context.Orders
+                .OrderByDescending(o => o.CreatedAt)
+                .FirstAsync(o => o.Quantity > 1);
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return _context.Orders
+                .Where(o => o.User.Status == UserStatus.Active)
+                .OrderByDescending(o => o.CreatedAt)
+                .ToListAsync();
+        }
+    }
+}
