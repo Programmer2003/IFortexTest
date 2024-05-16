1df57ce90a52359d6d5f86fcc5bf50033f608c40
Christian Repnik <kris18star@gmail.com>
2024-05-16 17:07:59 +0300

    User Service

---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context) => _context = context;
+
+        public Task<User> GetUser()
+        {
+            return _context.Users.FirstAsync(u =>
+                u.Id == _context.Orders
+                .GroupBy(o => o.UserId)
+                .OrderByDescending(o => o.Sum(o => o.Quantity * o.Price))
+                .Select(r => r.Key)
+                .First()
+            );
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return _context.Users
+                .Where(u =>
+                    u.Orders.Any(o => o.Status == OrderStatus.Paid && o.CreatedAt.Year == 2010)
+                 )
+                .ToListAsync();
+        }
+    }
+}
